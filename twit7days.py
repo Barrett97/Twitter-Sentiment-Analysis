e2a9d6efa800ee30142f45e2a992389e5d28ff25
Barrett97 <tranbarrett@gmail.com>
2020-08-02 00:24:13 -0400

    removed access keys

--- twit7days.py
+++ twit7days.py
@@ -34,36 +34,10 @@ from tweepy import API
 from tweepy import Cursor
 from tweepy import Stream
 
-
-# consumer_key="K7dHcWL94je6OqIFXFANPtxei"
-# consumer_secret="9Y14t2et6OmLUdXI14KUfUoGnKl5uzTeisTrMG1bHhSUZ4VJAK"
-# access_token="1063443082992066561-4hz7NTsrSBISbAUwvsBoNBUdwg5bmD"
-# access_token_secret="E5j4DDqXjrrOrrvbY4YDIxh0heJCpxC7rFEEZVdvtJ3R7"
-
-# consumer_key2="qxE2h8gsxiRKxhBji3qtALfW1"
-# consumer_secret2="LcqRdZGml2Th9yRGSW1OcV0bhG6AN5g3o6wW72Ra70WCtJHMxv"
-# access_token2="1063443082992066561-6OTM7AACJhKPJ0OTMdeOvnlX2DrLMZ"
-# access_token_secret2="ooyl84dnKUDgt0CnfbBQlWhF0VvuLvci3V0Z94llyD8wI"
-
-# consumer_key="UwMox6N9xrdVcY5hZvZGBLelJ"
-# consumer_secret="privea4gPdlkn7JfXKYzxHDCmDbVYNUZrc9Mn3YVpg1FkdIlPI"
-# access_token="1063443082992066561-6TcWY4LzY21iGswNsEgDJgvLjBLE5H"
-# access_token_secret="oFPx6OW1eO7RswYD4DpMJ3bA4vFXWpufL85xCLNbk9SaD"
-
-# consumer_key="pxBKWrfEXRJjOYfSI35VDp3Cj"
-# consumer_secret="D2djgEfkFktKjDvx1Z3EOJqsTPzdiHaX9iqqoIqvIjz3BDhqgq"
-# access_token="1063443082992066561-bg4Z4diMRFXc6OMLOrDh3b7XqQuF4N"
-# access_token_secret="m5cQFLV23meXq8lxbaIc21arj8jfNHOyCTAMLd9XC0Tev"
-
-# consumer_key="xITAF3eKITbF4L9PqRnNsF43t"
-# consumer_secret="Jj2nGRBJnCLcojVR5YSvBdjsgQTdGUXFU3i1HoW4LKd71wx6vD"
-# access_token="1063443082992066561-2A6Fr13CdloYDqV0ID1J5yki5h2z8T"
-# access_token_secret="O6fsawfAALlL08HXLBhnMEgC3O9HlVeK1Eqsc4Zru39bq"
-
-consumer_key="LhNkkGjmugupq1sxtMwCjKw4L"
-consumer_secret="HEqD6gnTZWFkUNgNBHPvSRlCYVbBLcCWIML9QWmalA40f4424C"
-access_token="1063443082992066561-JVsH7joANNYvDatpXkWI4AmCkKM2xw"
-access_token_secret="2K2XXJ3ljSBf13StwVnGxxkHlGc7KWoCrraB5zYX8aDB4"
+consumer_key=
+consumer_secret=
+access_token=
+access_token_secret=
 
 # ==== setup twitter connection ====
 auth = OAuthHandler(consumer_key, consumer_secret)
